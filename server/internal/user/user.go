4ce32ee2cac331edf22895ab425ffae62dfe177d
HemlockPham7 <london1952hoang@gmail.com>
2024-11-01 00:09:19 +0700

    Chore: Tidy

--- server/internal/user/user.go
+++ server/internal/user/user.go
@@ -22,14 +22,14 @@ type CreateUserRes struct {
 }
 
 type LoginUserReq struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `json:"email" db:"email"`
+	Password string `json:"password" db:"password"`
 }
 
 type LoginUserRes struct {
 	accessToken string
-	ID          string `json:"id"`
-	Username    string `json:"username"`
+	ID          string `json:"id" db:"id"`
+	Username    string `json:"username" db:"username"`
 }
 
 type Repository interface {
